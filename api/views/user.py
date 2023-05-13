0ee45098fa2f7cff1ad09889cf66d58969dcfdbd
Victor Zalabardo Fernandez <v.zalabardo@gmail.com>
2023-05-13 14:23:20 +0200

    joinGame Code check

--- api/views/user.py
+++ api/views/user.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from api.models.game import Game
 from api.models.user import CustomUser
 from api.serializers.user_serializer import UserSerializer
 
@@ -13,5 +14,21 @@ class UserViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['get'])
     def join_game(self, request, pk=None):
+        user = CustomUser.objects.get(pk=pk)
 
+        if "code" in request.data:
+            code = request.data['code'];
+            games_qs = Game.objects.filter(code=code)
+
+            if games_qs.exists():   # El objeto existe, puedes acceder a él
+                gameObject = games_qs.first()
+                if(gameObject)
+
+            else:             # El objeto no existe
+                return Response({'errorCode': 1 ,'message': 'Sala no encontrada'})
+            pass
+
+        else:
+
+            pass
         return Response({'message': 'Hello, world!'})
