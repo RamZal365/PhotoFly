d07f8b52a36aa22362fe016cfaca4f2056539f3a
Ramon Zalabardo <ramon.zalabardo.bosch@gmail.com>
2023-05-13 13:17:46 +0200

    Fields not required

--- api/serializers/user_serializer.py
+++ api/serializers/user_serializer.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from api.models.user import CustomUser
+from api.serializers.game_serializer import GameSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
-
+    game = GameSerializer(allow_null=True)
+    score = serializers.IntegerField(allow_null=True)
     class Meta:
         model = CustomUser
         fields = '__all__'
