ea5eb4e128918b4557425659cef8319293a094a9
Ramon Zalabardo <ramon.zalabardo.bosch@gmail.com>
2023-05-13 11:49:11 +0200

    Added fields and created migrations

--- api/models/user.py
+++ api/models/user.py
@@ -6,5 +6,5 @@ from api.models.game import Game
 
 class CustomUser(User):
     game = models.ForeignKey(Game, related_name='users', on_delete=models.CASCADE)
-    score = models.IntegerField()
+    score = models.IntegerField(null=True)
 
